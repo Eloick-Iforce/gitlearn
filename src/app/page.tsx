508d6cb738bece0ebe1c65e482295e052586f0df
Eloick-Iforce <eloick.mickisz@protonmail.com>
2023-11-03 22:36:50 +0100

    Add Git command search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,54 @@
-import Link from "next/link";
+"use client";
 
-export default function HomePage() {
+import React, { useState } from "react";
+import gitCommands from "./gitCommands.json";
+
+function App() {
+  const [language, setLanguage] = useState("fr");
+  const [search, setSearch] = useState("");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <div className="mx-auto flex max-w-full flex-wrap items-start justify-around rounded-xl bg-white p-6 shadow-md">
+      <h1 className="w-full text-center text-xl font-bold">
+        Apprendre les commandes Git
+      </h1>
+      <select
+        value={language}
+        onChange={(e) => setLanguage(e.target.value)}
+        className="mb-4"
+      >
+        <option value="fr">Français</option>
+        <option value="en">English</option>
+      </select>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Rechercher une commande"
+        className="mb-4 rounded border p-2"
+      />
+      {Object.entries(gitCommands).map(([category, commands]) => (
+        <div
+          key={category}
+          className="m-2 flex w-64 flex-col rounded bg-gray-200 p-4 shadow-lg"
+        >
+          <h2 className="mb-2 text-lg font-semibold">{category}</h2>
+          {commands
+            .filter(
+              (cmd) =>
+                cmd.command.includes(search) ||
+                cmd.description[language].includes(search),
+            )
+            .map((cmd, index) => (
+              <div key={index}>
+                <h3 className="text-md font-semibold">{cmd.command}</h3>
+                <p className="text-gray-800">{cmd.description[language]}</p>
+              </div>
+            ))}
         </div>
-      </div>
-    </main>
+      ))}
+    </div>
   );
 }
+
+export default App;
